1104c7bb09d3637b416cf9263b328d61aca6bd8a
Inna_IT <leto2077@gmail.com>
2023-02-11 22:47:39 +0300

    задача с вводом числа дня недели

--- C-sem1/Program.cs
+++ C-sem1/Program.cs
@@ -18,16 +18,43 @@
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
 
-Console.Write("Введите число 1: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число 2: ");
-int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число 1: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число 2: ");
+// int b = Convert.ToInt32(Console.ReadLine());
 
-if (a == b * b)
+// if (a == b * b)
+// {
+//     Console.Write("Да,число a есть квадратный корень b");
+// }
+// else
+// {
+//     Console.Write("НЕТ,число b не есть квадратный корень a");
+// }
+
+// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+Console.Write("Введите число от 1 до 7: ");
+int result = Convert.ToInt32(Console.ReadLine());
+if (result == 1)
 {
-    Console.Write("Да,число a есть квадратный корень b");
+    System.Console.WriteLine("Понедельник");
+}
+else if (result == 2)
+{
+    System.Console.WriteLine("Вторник");
+}
+else if (result == 3)
+{
+    System.Console.WriteLine("Среда");
+}
+else if (result == 4)
+{
+    System.Console.WriteLine("Chetverg");
 }
 else
 {
-    Console.Write("НЕТ,число b не есть квадратный корень a");
+    System.Console.WriteLine("Нет такого дня недели!");
 }
