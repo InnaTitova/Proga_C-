409d707e45c4148f32e3f367acf36aee81be2d19
Inna_IT <leto2077@gmail.com>
2023-02-11 23:24:34 +0300

    задача на остаток от трехзначного числа

--- C-sem1/Program.cs
+++ C-sem1/Program.cs
@@ -94,9 +94,25 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 // 2 -> " -2, -1, 0, 1, 2"
 
-Console.Write("Введите число: ");
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// for (int i = -N; i <=N; i++)
+// {
+//     System.Console.Write(i + " ");
+// }
+
+// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+System.Console.Write("Введите трехзначное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-for (int i = -N; i <=N; i++)
+if (N > 99 && N < 1000)
 {
-    System.Console.Write(i + " ");
+    System.Console.WriteLine(N%10);
+}
+else
+{
+    System.Console.WriteLine("Введено не 3х значное число");
 }
