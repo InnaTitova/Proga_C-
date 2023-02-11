0a6999a483aa2571934c7e9652c25f9a49223432
Inna_IT <leto2077@gmail.com>
2023-02-11 22:30:29 +0300

    сравнивали квадрат первого есть ли квадрат второго

--- C-sem1/Program.cs
+++ C-sem1/Program.cs
@@ -1,12 +1,33 @@
-﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
-// на само себя).
+﻿// // Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// // на само себя).
 
-// Например:
-// 4 -> 16 
-// -3 -> 9 
-// -7 -> 49
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int result = number * number;
-System.Console.WriteLine("Квадрат числа = " + result);
-System.Console.WriteLine($"Квадрат числа = {result} текст"); //вывод через форматированную сроку
+// // Например:
+// // 4 -> 16 
+// // -3 -> 9 
+// // -7 -> 49
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int result = number * number;
+// System.Console.WriteLine("Квадрат числа = " + result);
+// System.Console.WriteLine($"Квадрат числа = {result} текст"); //вывод через форматированную сроку
+
+
+// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.Write("Да,число a есть квадратный корень b");
+}
+else
+{
+    Console.Write("НЕТ,число b не есть квадратный корень a");
+}
