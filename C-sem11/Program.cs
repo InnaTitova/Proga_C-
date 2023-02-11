b12e782fb669932810055d7c9fbf8fdd4a4ca226
Inna_IT <leto2077@gmail.com>
2023-02-11 23:42:57 +0300

    save github

---
+++ C-sem11/Program.cs
@@ -0,0 +1,10 @@
+﻿// // Например:
+// // 4 -> 16 
+// // -3 -> 9 
+// // -7 -> 49
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int result = number * number;
+// System.Console.WriteLine("Квадрат числа = " + result);
+// System.Console.WriteLine($"Квадрат числа = {result} текст"); //вывод через форматированную сроку
+
